ee546394511b90d73aeb73c3f9db877be91977b3
mithundnair <mithudileep1234567@gmail.com>
2024-07-11 20:26:07 +0530

    bookingmodal

--- script/packages.js
+++ script/packages.js
@@ -6,3 +6,20 @@ function BookTickets() {
     });
     myModal.show();
 }
+
+function showUserForm(user){
+    document.getElementById('userform').classList.add('hidden');
+    document.getElementById('childform').classList.add('hidden');
+    document.getElementById('under').classList.add('hidden');
+
+     // Show the selected user form
+     document.getElementById('userform').classList.remove('hidden');
+}
+
+function showChildrenForm() {
+    document.getElementById('userform').classList.add('hidden');
+
+    //show the child form
+    document.getElementById('under').classList.remove('hidden');
+    document.getElementById('childform').classList.remove('hidden');
+}
